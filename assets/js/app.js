2cfc73bb1d85e859efb7e79b7683985e4f306568
unknown <sanjeevmaharjan19@gmail.com>
2021-11-08 21:07:54 +0545

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+
+
+function typingEffect(){
+	let text = "I am a web developer";
+	let speed = 200;
+	let i = 0
+	let timer = setInterval(function(){
+		if(i<text.length){
+			document.getElementById('demo').innerHTML += text.charAt(i);
+			i++;
+		}else{
+			document.getElementById('demo').innerHTML = " ";	
+			i = 0;
+		}
+	},speed);
+	
+}
+
+typingEffect();
+
+const menuDisplay = document.querySelector(".menu-button");
+const sideNav = document.querySelector(".side-nav");
+menuDisplay.addEventListener("click",function(){
+		sideNav.classList.toggle("left-align");
+
+})
