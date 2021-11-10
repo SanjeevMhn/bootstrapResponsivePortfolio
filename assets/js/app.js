a3d8810edbb1caeace4414304a3c19494afc286d
unknown <sanjeevmaharjan19@gmail.com>
2021-11-10 16:39:09 +0545

    almost done need to add the gallery view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,15 @@
 
+console.log(window.innerWidth);
+
+const backToTop = document.querySelector(".back-to-top");
+window.addEventListener("scroll",function(){
+	if(window.scrollY>0){
+		backToTop.style.display = "flex";
+	}else{
+		backToTop.style.display = "none";
+	}
+})
+
 
 function typingEffect(){
 	let text = "I am a web developer";
@@ -20,7 +31,15 @@ typingEffect();
 
 const menuDisplay = document.querySelector(".menu-button");
 const sideNav = document.querySelector(".side-nav");
+const hamBurger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+
+
 menuDisplay.addEventListener("click",function(){
 		sideNav.classList.toggle("left-align");
+		hamBurger.classList.toggle("noDisplay");
+		close.classList.toggle("display");
 		//console.log("clicked");
 })
+
+
